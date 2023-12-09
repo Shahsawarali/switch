cac0b483533264fcadffa7be3537966aa0eb0158
IlOvecompUter1$t <shahva2022@gmail.com>
2023-12-09 10:53:06 +0500

    switch

---
+++ switch.js
@@ -0,0 +1,65 @@
+// day = prompt("Enter name of day")
+// switch(day){
+//     case "Saturday":
+//         alert("weeekend")
+//         break;
+//     case "Sunday":
+//         alert("weekend")
+//         break;
+//     case "Monday":
+//         alert("weekstart")
+//         break;
+//     case "Tuesday":
+//         alert ("weekday")
+//         break;
+//     case "Wednesday":
+//         alert ("weekday")
+//         break;
+//     case "Thursday":
+//             alert ("weekday")
+//             break;
+//     case "Friday":
+//             alert ("lastweekday")
+
+//     default:
+//         alert("Enter a valid day")
+// }
+
+
+
+
+// age = prompt("Enter your age")
+// switch(true){
+//     case age>0 && age <=2:
+//       alert("You are a toddler");
+//       break;
+//       case age>3 && age <=14:
+//         alert("You are a kid");
+//         break;
+//         case age>15 && age <=20:
+//       alert("You are a teenager");
+//       break;
+//       case age>21 && age <=40:
+//       alert("You are a young");
+//       break;
+//       case age>41 && age <=60:
+//       alert("You are a growup man");
+//       break;
+//     default:
+//         alert("Oldman")
+// }
+
+
+
+
+
+var rightNow = new Date()
+
+alert(rightNow)
+alert(rightNow.getMonth())
+alert(rightNow.getDay())
+alert(rightNow.getDate())
+alert(rightNow.getFullYear())
+alert(rightNow.getHours())
+alert(rightNow.getMinutes())
+alert(rightNow.getTime())
